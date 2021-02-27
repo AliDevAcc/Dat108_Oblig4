fb5e06b903de641595a7bb335597dcda6c0535c9
h578043 <42607311+h578043@users.noreply.github.com>
2021-02-27 21:38:20 +0100

    Add files via upload

---
+++ Dat108_Oblig4/Oblig4/DAT108GraphAndApproximationAlgs/src/no/hvl/dat108/graphs/DepthFirstSearch.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package no.hvl.dat108.graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * @author tdoy
+ *
+ */
+public class DepthFirstSearch {
+
+	private String source;
+	private Graph graph;
+	
+	public DepthFirstSearch(Graph graph, String source) {
+		
+		this.graph = graph;
+		this.source = source;
+	}
+	
+	public Set<String> doDepthFirstSearch(){
+		
+		Set<String> visited = new LinkedHashSet<String>();
+		Stack<String> stack = new Stack<String>();
+		
+		//push the source in the stack
+		
+		// implement me
+				
+		return visited;
+	}
+	
+	public Set<String> doDepthFirstSearchRecursive(){
+		
+		Set<String> visited = new LinkedHashSet<String>();
+		List<Node> nodes = new ArrayList<Node>(graph.getAdjNodes().keySet());
+		
+		// use nodes
+		Node src = nodes.get(nodes.indexOf(new Node(source)));
+		
+		// call dfsRecursive method here
+		dfsRecursive(nodes,src,visited);		
+		return visited;
+	}
+	
+	public void dfsRecursive(List<Node> nodes, Node src, Set<String> visited) {
+		// implement me
+		src.setVisited(true);
+		visited.add(src.getName());
+
+		for (Node node : graph.getAdjacentNodes(src.getName())) {
+		node = nodes.get(nodes.indexOf(node));
+		if (!node.isVisited()) {
+			dfsRecursive(nodes, node, visited);
+		}
+
+	}
+		
+		
+	}
+}
+
---
+++ Dat108_Oblig4/Oblig4/DAT108GraphAndApproximationAlgs/src/no/hvl/dat108/graphs/Test.java
@@ -0,0 +1,6 @@
+package no.hvl.dat108.graphs;
+
+public class Test {
+public static void main(String[] args) {
+}
+}
